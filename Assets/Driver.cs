77d3ba26078c02b70208aef185403e09a110a5b6
Katerina Kolchakovska <katerina.kolchakovska@gmail.com>
2024-05-21 19:59:52 +0200

    Initial commit

---
+++ Assets/Driver.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 0.1f;
+    [SerializeField] float moveSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, turnSpeed);
+        transform.Translate(0, moveSpeed, 0 );
+    }
+}
