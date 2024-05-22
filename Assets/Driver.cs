a0c1926b4e46abb938df2c419ff28bb121b5ffd1
Katerina Kolchakovska <katerina.kolchakovska@gmail.com>
2024-05-22 14:45:11 +0200

    Delivery Driver

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float turnSpeed = 0.1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float turnSpeed = 1f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
+  
     void Update()
     {
-        transform.Rotate(0, 0, turnSpeed);
-        transform.Translate(0, moveSpeed, 0 );
+        float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -turnAmount);
+        transform.Translate(0, moveAmount, 0 );
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "SpeedUp")
+        {
+            Debug.Log("Boost");
+            moveSpeed = boostSpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveSpeed = slowSpeed;
     }
 }
